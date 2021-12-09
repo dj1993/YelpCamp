21d9c22741e0cc0e523a14ff319528bc49216b99
dj1993 <dj.lfv.rodrigues@gmail.com>
2021-12-09 11:55:35 +0000

    Added error handling

---
+++ schemas.js
@@ -0,0 +1,11 @@
+const Joi = require('joi')
+const campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required()
+    }).required()
+})
+
+module.exports = campgroundSchema
